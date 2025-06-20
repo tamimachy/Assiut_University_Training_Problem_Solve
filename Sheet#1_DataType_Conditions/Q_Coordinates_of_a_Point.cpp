ab2407e53bdfd8a67cf1d4323eda55616447bcfb
USER <tamimachy9897@gmail.com>
2025-06-21 00:00:17 +0600

    solving

---
+++ Sheet#1_DataType_Conditions/Q_Coordinates_of_a_Point.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float X, Y;
+    cin >> X >> Y;
+    if(X==0 && Y == 0){
+        cout <<"Origem";
+    }
+    else if(X==0){
+        cout <<"Eixo Y";
+    }
+    else if(Y==0){
+        cout <<"Eixo X";
+    }else if(X>0 && Y>0){
+        cout << "Q1";
+    }else if(X<0 && Y>0){
+        cout << "Q2";
+    }else if(X<0 && Y<0){
+        cout << "Q3";
+    }else
+        cout << "Q4";
+    return 0;
+}
