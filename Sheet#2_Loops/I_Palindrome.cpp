fe54dad2c4fbb90d100beb604b9f2f4a026ee36b
USER <tamimachy9897@gmail.com>
2025-07-16 00:01:14 +0600

    today

---
+++ Sheet#2_Loops/I_Palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, re = 0;
+    cin >> n;
+    int temp = n;
+    while(temp>0){
+        re = re * 10 + temp%10;
+        temp = temp/10;
+    }
+    cout << re << "\n";
+    if(re == n){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+
+    return 0;
+}
