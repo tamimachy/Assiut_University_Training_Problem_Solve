2063da2e23cb556c9564999f538db386b4c66bc0
USER <tamimachy9897@gmail.com>
2025-09-27 23:29:18 +0600

    smallest number

--- Sheet #3_Arrays/I_Smallest_Pair.cpp
+++ Sheet #3_Arrays/I_Smallest_Pair.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int t, n, i,j, temp = 0;
+    int t, n, i,j;
     cin >> t;
-    int Test[t];
-    for(i=0; i<t; i++){
-        cin >> Test[i];
+    while(t--){
         cin >> n;
         int A[n];
-        for(i=0; i<n; i++){
+        for(i=0;i<n;i++){
             cin >> A[i];
         }
-    }
-    for(i=1; i<n-1; i++){
-       for(j=i+1;j<n-i;j++){
-        if(A[j]>A[j+1]){
-          swap(A[j],A[j+1]);
+        int ans = INT_MAX;
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                ans = min(ans,A[i]+A[j]+j-i);
+            }
         }
-       }
+        cout << ans << endl;
     }
-    for(i=0; i<n; i++){
-        cout << A[i] << " ";
-    }
+
     return 0;
 }
