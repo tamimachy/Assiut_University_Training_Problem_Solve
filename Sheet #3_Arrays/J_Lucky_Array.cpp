28b6f45e49d4f518b2dcc9026da2809a7f34cd9b
USER <tamimachy9897@gmail.com>
2025-10-01 23:04:42 +0600

    lucky array

--- Sheet #3_Arrays/J_Lucky_Array.cpp
+++ Sheet #3_Arrays/J_Lucky_Array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main() {
     int n, i,j;
@@ -6,6 +7,20 @@ int main() {
     int A[n];
     for(i=0;i<n;i++)
         cin>>A[i];
+    sort(A,A+n);
+    int counter = 0;
+    for(i=0; i<n; i++){
+        if(A[i]==A[0]){
+            counter++;
+        }
+        else
+            break;
+    }
+    if(counter%2){
+        cout << "Lucky"<<endl;
+    }else{
+        cout << "Unlucky" << endl;
+    }
 
     return 0;
 }
