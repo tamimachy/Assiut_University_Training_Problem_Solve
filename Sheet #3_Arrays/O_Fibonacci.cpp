10863ce5c0b5e6861bb3832b1ecf62b4c88ae9ab
USER <tamimachy9897@gmail.com>
2025-10-11 22:30:03 +0600

    single fibonacchi value

---
+++ Sheet #3_Arrays/O_Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n, n1=0, n2=1, next_fib;
+    cin >> n;
+    if(n==1){
+        next_fib = n1;
+    }else{
+        next_fib = n2;
+        for(int i =0;i<n-2;i++){
+            next_fib = n1+n2;
+            n1=n2;
+            n2=next_fib;
+        }
+    }
+    cout << next_fib << endl;
+}
