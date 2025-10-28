ef7526129054922e1565ffa33e63a337ad277bbe
USER <tamimachy9897@gmail.com>
2025-10-28 19:36:25 +0600

    mirror array show

---
+++ Sheet #3_Arrays/W_Mirror_Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n,m,i,j;
+    cin >> n >> m;
+    int a[n][m];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            cin >> a[i][j];
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=m-1;j>=0;j--){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
