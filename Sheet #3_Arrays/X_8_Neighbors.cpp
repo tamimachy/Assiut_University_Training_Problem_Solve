626ab1247ec904d2f1d89c108ec7861034f82255
USER <tamimachy9897@gmail.com>
2026-05-15 21:26:49 +0600

    Sheet 3_ X. 8 Neighbors added

--- Sheet #3_Arrays/X_8_Neighbors.cpp
+++ Sheet #3_Arrays/X_8_Neighbors.cpp
@@ -5,16 +5,21 @@ int main(){
     int n,m,x,y,i,j;
     cin >> n >> m;
     char a[n][m];
-    for(i=1;i<=n;i++){
-        for(j=1;j<=m;j++){
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
             cin >> a[i][j];
         }
     }
     cin>>x>>y;
-    if(a[x][y-1] !='.' && a[x][y+1] !='.'&& a[x-1][y] !='.'&& a[x+1][y] !='.'&& a[x-1][y-1] !='.'&& a[x-1][y+1] !='.'&& a[x+1][y+1] !='.'&& a[x+1][y-1] !='.'){
-        cout<<"yes";
-    }else{
-        cout<<"no";
-    }
+    x=x-1;
+    y=y-1;
+    if(a[x-1][y-1]!='.' && a[x-1][y]!='.'&& a[x-1][y+1]!='.'&&
+        a[x][y-1]!='.'&& a[x][y+1]!='.'&&
+        a[x+1][y-1]!='.'&& a[x+1][y]!='.'&& a[x+1][y+1]!='.')
+            {
+                cout<<"yes";
+            }else{
+                cout<<"no";
+            }
     return 0;
 }
