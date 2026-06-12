484efbbdd54439e47519d98bfcabe0f80b0bb662
USER <tamimachy9897@gmail.com>
2026-06-12 22:05:18 +0600

    last change

--- Difficulty_800/B_Palindrome_Twelve_&_Two_Terms.cpp
+++ Difficulty_800/B_Palindrome_Twelve_&_Two_Terms.cpp
@@ -1,38 +1,27 @@
 #include<bits/stdc++.h>
 #include<algorithm>
 #include <vector>
-/*int palindrome(long long arr[]){
-    int left = 0;
-    int right = arr.size() - 1;
-    while(left < right){
-        if(arr[left] != arr[right]){
-            return -1;
-        }
-        left +=1;
-        right -=1;
-    }
-    return 1;
-} */
 using namespace std;
+void solve(){
+    long long n;
+    cin >> n;
+    int reminder = n%12;
+    if(n==10)
+        cout << -1 << endl;
+    long a = -1;
+    if(reminder<=9)
+        a = reminder;
+    else if(reminder == 10)
+        a = 22;
+    else
+        a = 11;
+    cout << a << " " << n-a << endl;
+}
 int main(){
-    int t, a,b,i;
+    int t;
     cin >> t;
-    vector<long long> n(t);
-    for(i=0;i<t;i++){
-        cin >> n[i];
-    }
-    for(i=0;i<t;i++){
-        a = i+1;
-        b = i%12;
-        if(n[i]==a && b == 0){
-            cout << a << " " << b << endl;
-        }else if(n[i] == 10){
-            cout << -1 << endl;
-        }else if(n[i]%12 == 10){
-            b = 12*(i+1);
-            //a = n[i] - b;
-            cout << " " << b << endl;
-        }
+    while(t--){
+        solve();
     }
     return 0;
 }
