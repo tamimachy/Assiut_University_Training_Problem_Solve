0b194a55257899502818fa756d37a4f211e79c35
USER <tamimachy9897@gmail.com>
2026-06-13 22:32:34 +0600

    Solved 2234B

--- Difficulty_800/B_Palindrome_Twelve_&_Two_Terms.cpp
+++ Difficulty_800/B_Palindrome_Twelve_&_Two_Terms.cpp
@@ -5,17 +5,21 @@ using namespace std;
 void solve(){
     long long n;
     cin >> n;
-    int reminder = n%12;
+    // int reminder = n%12;
     if(n==10)
+    {
         cout << -1 << endl;
-    long a = -1;
-    if(reminder<=9)
-        a = reminder;
-    else if(reminder == 10)
-        a = 22;
-    else
-        a = 11;
-    cout << a << " " << n-a << endl;
+        return;
+    }
+    long long A;
+    long long B;
+    A = n%12;
+    B=(n-A);
+    if(A==10){
+        A+=12;
+        B-=12;
+    }
+    cout << A << " " << B << endl;
 }
 int main(){
     int t;
